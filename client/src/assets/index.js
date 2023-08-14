1864e7be983f00fe71ee926607757b95c75e9736
Abraar07 <mohammedabraar05@gmail.com>
2023-08-14 21:22:25 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,13 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import mlogo from './mlogo.svg';
+import mimg from './mimg.png';
+
+export {
+  download,
+  logo,
+  preview,
+  mlogo,
+  mimg,
+};
