54f56222b546681d63c7e6860c48a993f34229b0
job kilonzo <32642682+jobkilonzo@users.noreply.github.com>
2023-12-24 11:24:19 +0300

    Update dalleRoutes.js
    Droped configuration function, edited response from openai fun call

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { OpenAIApi } from 'openai';
 
 dotenv.config();
 
@@ -10,7 +10,9 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAIApi({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL-E!' });
@@ -20,14 +22,14 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
